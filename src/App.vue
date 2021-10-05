bac8378b48772c745311de705e68fec5f105f7cc
oliver <olivier.codes@gmail.com>
2021-10-05 15:51:46 +0200

    add template

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
-    
+    <Header />
+    <Introduction />
+    <Main />
+    <Player />
   </div>
   <router-view/>
 </template>
+
+<script>
+import Header from '@/components/Header'
+import Introduction from '@/components/Introduction'
+import Main from '@/components/Main'
+import Player from '@/components/Player'
+
+
+export default {
+  name: 'App',
+  components: { Header,Introduction, Main, Player}
+}
+</script>
 
 <style>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- Header -->
+  <header id="header" class="bg-gray-50">
+    <nav class="container mx-auto flex justify-start items-center py-5 px-4">
+      <!-- App Name -->
+      <svg
+        class="w-6 h-6 mr-5 text-green-500"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M9 19V6l12-3v13M9 19c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zm12-3c0 1.105-1.343 2-3 2s-3-.895-3-2 1.343-2 3-2 3 .895 3 2zM9 10l12-3"
+        ></path>
+      </svg>
+
+      <a class="text-dark font-bold uppercase text-2xl mr-4" href="#">SpotifVue</a>
+
+      <div class="flex flex-grow items-center">
+        <!-- Primary Navigation -->
+        <ul class="flex flex-row mt-1">
+          <!-- Navigation Links -->
+          <li>
+            <a class="px-2 text-dark" href="#">Se connecter / S'enregistrer</a>
+          </li>
+          <li>
+            <a class="px-2 text-dark" href="#">Admin</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style></style>
